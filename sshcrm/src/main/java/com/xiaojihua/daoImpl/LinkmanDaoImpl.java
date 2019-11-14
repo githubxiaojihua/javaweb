1d543ce24fc34c363f8ad02c11ec91947353249e
xiaojihua <2542213767@qq.com>
2019-11-14 11:30:31 +0800

    20191114

--- sshcrm/src/main/java/com/xiaojihua/daoImpl/LinkmanDaoImpl.java
+++ sshcrm/src/main/java/com/xiaojihua/daoImpl/LinkmanDaoImpl.java
@@ -43,12 +43,21 @@ public class LinkmanDaoImpl implements LinkmanDao {
 
     @Override
     public List<Linkman> findPage(DetachedCriteria dc, int start, int pageSize) {
+        /**
+         * 设置为Null主要是因为，service端先调用findCount方法，
+         * findCount方法会设置一个Projection。
+         * 因为在同一个DC里面因此在调用本方法的时候还会保留，导致
+         * hibernate生成的sql还是查询select count(*)这样的，
+         * 只有设置null清空projection后才能根据条件生成
+         * select * from Linkman where ....这样的
+         */
         dc.setProjection(null);
         return (List<Linkman>)template.findByCriteria(dc,start,pageSize);
     }
 
     @Override
     public int findCount(DetachedCriteria dc) {
+        //设置查询聚合函数。生成的语句select count(*).....
         dc.setProjection(Projections.rowCount());
         List<Long> list = (List<Long>)template.findByCriteria(dc);
         return list.get(0).intValue();
